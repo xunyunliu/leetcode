71b8e1cc8c71ed57094efd2fec9a17c7014ed412
Xunyun Liu <xunyunliu@users.noreply.github.com>
2017-10-26 18:13:25 +1100

    update

--- src/Solution5.java
+++ src/Solution5.java
@@ -1,2 +1,43 @@
 public class Solution5 {
+
+    public static String longestPalindrome(String s) {
+
+
+    }
+
+    public static String longestPalindrome_TLE(String s) {
+        int len = s.length();
+        if(len==0)
+            return null;
+
+        int max = -1;
+        int max_i = -1;
+        int max_j = -1;
+        for (int i = 0; i < len; i++) {
+            for (int j = i; j < len; j++) {
+                int tmp_i = i;
+                int tmp_j = j;
+                while (tmp_i <= tmp_j) {
+                    if (s.charAt(tmp_i) != s.charAt(tmp_j))
+                        break;
+                    else {
+                        tmp_i++;
+                        tmp_j--;
+                    }
+                }
+                if (tmp_i > tmp_j && j - i + 1 > max) {
+                    //System.out.println(max);
+                    max = j - i + 1;
+                    max_i = i;
+                    max_j = j;
+                }
+            }
+        }
+        return s.substring(max_i,max_j+1);
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(longestPalindrome("dddddd"));
+    }
 }
